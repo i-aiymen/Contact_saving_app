43bf093c606625c7df463a5c8d9d3ebd0a63c449
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 13:43:41 +0530

    Updated: the favourite page where the favourite data will be seen

--- src/pages/Favourite.js
+++ src/pages/Favourite.js
@@ -1,11 +1,26 @@
+import Contact from "../components/Contact";
 
-
-function Favourite() {
-    return (
-        <div>
-            
+function Favourite({ contacts, deleteContact, favToggle }) {
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5">
+          {contacts.map((singleContact) => {
+            return (
+              singleContact.fav && (
+                <Contact
+                  key={singleContact.id}
+                  contact={singleContact}
+                  deleteContact={deleteContact}
+                  favToggle={favToggle}
+                />
+              )
+            );
+          })}
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Favourite
+export default Favourite;
