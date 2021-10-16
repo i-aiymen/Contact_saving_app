cc1beb3bc974976073b89f9c03350cde79f2b037
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 12:14:26 +0530

    Passed: Data from Form to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Contact from "../components/Contact";
+import Form from "../components/Form";
 
-function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Home({ formSub }) {
+  return (
+    <div className="container my-5">
+      <div className="row justify-content-sm-center my-5">
+        <Form formSub={formSub} />
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
