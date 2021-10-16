a161a854473eb01f5d4d97de08bd0cc030ee179f
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 14:26:52 +0530

    Connected: everything with the server

--- src/App.js
+++ src/App.js
@@ -4,29 +4,83 @@ import Home from "./pages/Home";
 import Favourite from "./pages/Favourite";
 import NotFound from "./pages/NotFound";
 import Nav from "./components/Nav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const formSub = (data) => {
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const contactsFormServer = await fetchContacts();
+      setContacts(contactsFormServer);
+    };
+
+    getContacts();
+  }, []);
+  const formSub = async (data) => {
     //console.log(data);
-    setContacts([...contacts, data]);
+    const res = await fetch("http://localhost:3004/contacts", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const newdata = await res.json();
+    if (res.ok) {
+      setContacts([...contacts, newdata]);
+    }
   };
 
-  const deleteContact = (id) => {
-    let newContact = contacts.filter((singleContact) => {
-      return singleContact.id !== id;
-    });
-    setContacts(newContact);
+  const fetchContacts = async () => {
+    const res = await fetch("http://localhost:3004/contacts");
+    const data = await res.json();
+
+    return data;
   };
 
-  const favToggle = (id) => {
-    let updatedContact = contacts.map((singleContact) => {
-      return singleContact.id === id
-        ? { ...singleContact, fav: !singleContact.fav }
-        : singleContact;
+  const deleteContact = async (id) => {
+    const res = await fetch(`http://localhost:3004/contacts/${id}`, {
+      method: "DELETE",
     });
-    setContacts(updatedContact);
+
+    if (res.status === 200) {
+      let newContact = contacts.filter((singleContact) => {
+        return singleContact.id !== id;
+      });
+
+      setContacts(newContact);
+    }
+  };
+
+  const getCon = async (id) => {
+    const res = await fetch(`http://localhost:3004/contacts/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
+  const favToggle = async (id) => {
+    const singleCon = await getCon(id);
+
+    const updTask = { ...singleCon, fav: !singleCon.fav };
+
+    const res = await fetch(`http://localhost:3004/contacts/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    if (res.status === 200) {
+      let updatedContact = contacts.map((singleContact) => {
+        return singleContact.id === id
+          ? { ...singleContact, fav: !singleContact.fav }
+          : singleContact;
+      });
+      setContacts(updatedContact);
+    }
   };
 
   return (
