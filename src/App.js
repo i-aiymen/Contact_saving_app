8989b36adbb0684aacd6ae94ec4d6f665ec64c2c
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-10-16 12:15:21 +0530

    Passed: Data from home to app.js and rendered

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Favourite from "./pages/Favourite";
 import NotFound from "./pages/NotFound";
 import Nav from "./components/Nav";
 
-
 function App() {
-  return(
+  const formSub = (data) => {
+    console.log(data);
+  };
+  return (
     <Router>
       <Nav />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home formSub={formSub} />
         </Route>
         <Route path="/favourite">
           <Favourite />
@@ -23,7 +25,6 @@ function App() {
       </Switch>
     </Router>
   );
-    
 }
 
 export default App;
